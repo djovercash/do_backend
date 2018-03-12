23af65c69d0a2cd6524491edaf75ae19246900dd
github username <github email address>
2018-03-12 09:02:35 -0400

    Email update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain               => 'gmail.com',
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
-  :enable_starttls_auto => true
+   :enable_starttls_auto => true
   }
 
   config.action_mailer.perform_caching = false
