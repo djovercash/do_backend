2cf359aa4a8593fc52edd915b2dd073f219d71b3
github username <github email address>
2018-03-12 09:28:15 -0400

    Update emailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,17 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :domain               => 'gmail.com',
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-   :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { host:'localhost', port: '3001/contact' }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+        :address => "smtp.gmail.com",
+        :port => 587,
+        :domain => 'localhost:3000/contact',
+        :user_name => ENV['gmail_username'],
+        :password  => ENV['gmail_password'],
+        :authentication => :plain,
+        :enable_starttls_auto => true
+    }
 
   config.action_mailer.perform_caching = false
 
