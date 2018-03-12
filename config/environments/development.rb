db19886815be32233dce38b389a12dcf47535a35
github username <github email address>
2018-03-12 09:15:49 -0400

    Update port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
-   :port                 => 587,
+   :port                 => 465,
    :domain               => 'gmail.com',
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
