848fd2e2cab359a37ca75258cfd19655197c3da3
github username <github email address>
2018-03-12 09:29:30 -0400

    3001

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
         :address => "smtp.gmail.com",
         :port => 587,
-        :domain => 'localhost:3000/contact',
+        :domain => 'localhost:3001/contact',
         :user_name => ENV['gmail_username'],
         :password  => ENV['gmail_password'],
         :authentication => :plain,
